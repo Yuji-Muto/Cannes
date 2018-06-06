d2234c75e34c096238f83efab9a0b4bef94c1564
qdvb <sqdvq>
2018-06-06 17:47:13 +0200

    début vue

---
+++ MVC/views/v_Connnexion.php
@@ -0,0 +1,11 @@
+<form action="" method="post" >
+
+   <p><label for="id">Identifiant</label> : <input id="id" type="text" name="id" /> 
+   
+   <label for="passwd">Mot de passe</label> : <input id="passwd" type="password" name="passwd" />
+   
+   <input type="submit" value="Se connecter" />
+   
+   </p>
+   
+</form>
---
+++ MVC/views/v_SaisieVip.php
@@ -0,0 +1,16 @@
+ <form>
+	<label for="nom">Nom</label>  <input type="text" name="nom" id="nom" />
+	<br>
+	<label for="prenom">Prénom</label>  <input type="text" name="prenom" id="prenom" />
+	<br>
+	<label for="metier">Métier</label>  <input type="email" name="metier" id="metier" />
+	<br> 
+	<label for="datenaiss">Date de naissance</label>  <input type="email" name="datenaiss" id="datenaiss" />
+	<br>   
+	<label for="nationalite">Nationalité</label>  <input type="email" name="nationalite" id="nationalite" />
+	<br>   
+	<label for="coeficient">Coeficient d'importance</label>  <input type="number" min="1" max="10" step="1" name="coeficient" id="coeficient" />
+	<br>
+	<input type="submit" value="Ajouter" />
+	   
+</form>
